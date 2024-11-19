e33a0cde797ca6a96622b12812ab8536a94df43c
ronny <v.ronny.harianto@gmail.com>
2024-11-19 16:24:39 +0700

    feat: add EnumHelper & JsonHelper

---
+++ NET.Starter.API/NET.Starter.API.Shared/Helpers/EnumHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace NET.Starter.API.Shared.Helpers
+{
+    internal static class EnumHelper
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+
+            if (field != null)
+            {
+                var attribute = (DescriptionAttribute?)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+
+                if (attribute != null) return attribute.Description;
+            }
+
+            return value.ToString();
+        }
+
+        public static List<TEnum> FilterEnumList<TEnum>(string? filterKey)
+            where TEnum : Enum
+        {
+            List<TEnum> result = [];
+
+            foreach (TEnum item in Enum.GetValues(typeof(TEnum)))
+            {
+                if (item.GetDescription().Contains(filterKey ?? string.Empty, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+    }
+}
