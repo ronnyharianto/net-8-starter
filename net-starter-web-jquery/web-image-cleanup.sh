cf25c3189357c976e313d9da49e396cc5d4b5a13
ronny <v.ronny.harianto@gmail.com>
2025-07-07 15:19:39 +0700

    feat: update github action for web and api

---
+++ net-starter-web-jquery/web-image-cleanup.sh
@@ -0,0 +1,9 @@
+prefix=net-starter-web
+echo "Cleaning up Docker images for: $prefix"
+
+docker images --format '{{.Repository}}:{{.Tag}} {{.CreatedAt}}' | \
+  grep "^$prefix:" | \
+  sort -rk2 | \
+  awk '{print $1}' | \
+  tail -n +6 | \
+  xargs -r docker rmi
