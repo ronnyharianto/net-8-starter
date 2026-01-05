ce2bfcc00b34692da3db769e6c6042a99031df58
ronny <v.ronny.harianto@gmail.com>
2026-01-05 13:35:30 +0700

    feat: add feature click enter to trigger filter

--- net-starter-web-jquery/NET.Starter.Web/wwwroot/js/filterHelper.js
+++ net-starter-web-jquery/NET.Starter.Web/wwwroot/js/filterHelper.js
@@ -1,24 +1,37 @@
 ﻿(function () {
-    function setupFilter(btnFilter, btnReset, filterContainer, gridPaging) {
-        if (!btnFilter || !btnReset || !filterContainer || !gridPaging) return;
+    function setupFilter($btnFilter, $btnReset, $filterContainer, $gridPaging) {
+        if (!$btnFilter || !$btnReset || !$filterContainer || !$gridPaging) return;
 
         const filter = history.state?.filter;
         if (filter) {
-            FormHelper.populateFormData(filterContainer, filter, null);
+            FormHelper.populateFormData($filterContainer, filter, null);
         }
 
-        btnFilter.on('click', function () {
-            const formDataObject = FormHelper.getFormData(filterContainer);
+        const $elFilter = $filterContainer.find(
+            'input[type="text"], input[type="number"], input[type="search"], input[type="date"]'
+        );
+
+        $btnFilter.on('click', function () {
+            const formDataObject = FormHelper.getFormData($filterContainer);
             history.pushState({ filter: formDataObject }, '', '?mode=' + CommonHelper.generateUUID());
 
-            gridPaging.api().draw();
+            $gridPaging.api().draw();
         });
 
-        btnReset.on('click', function () {
-            filterContainer.find('input,select').val(null).trigger('change');
+        $elFilter.on('keyup', function (e) {
+            if (e.isComposing) return;
+
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                $btnFilter.trigger('click');
+            }
+        });
+
+        $btnReset.on('click', function () {
+            $filterContainer.find('input,select').val(null).trigger('change');
             history.pushState(null, '', '?reset');
 
-            gridPaging.api().draw();
+            $gridPaging.api().draw();
         });
     }
 
